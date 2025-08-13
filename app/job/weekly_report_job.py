4ca005c54586f150859ab4022523738eab3819d3
prefer52 <prefer0502@gmail.com>
2025-08-13 19:56:23 +0900

    feat: 주간 종합 분석 완료 시 매매별 분석에 주간 종합 분석 외래키 업데이트

--- app/job/weekly_report_job.py
+++ app/job/weekly_report_job.py
@@ -16,12 +16,19 @@ logger = logging.getLogger(__name__)
 @traceable(name="주간 매매 종합 분석 cron job")
 async def weekly_report_job(app: FastAPI) -> None:
     db = SessionLocal()
-    suggestions = (db.query(WeeklyAnalysis.suggestion)
-                   .filter(func.yearweek(WeeklyAnalysis.date_time) == func.yearweek(func.curdate()))
-                   .all())
+    weekly_analyses = (db.query(WeeklyAnalysis)
+                       .filter(func.yearweek(WeeklyAnalysis.date_time) == func.yearweek(func.curdate()))
+                       .all())
 
-    weekly_report = await weekly_report_chain.ainvoke(prompt_parameter={"suggestions": suggestions})
+    weekly_report = await weekly_report_chain.ainvoke(
+        prompt_parameter={"suggestions": [weekly_analysis.suggestion for weekly_analysis in weekly_analyses]}
+    )
 
-    db.add(WeeklyReport(period=date.today(), summary=weekly_report.summary))
+    weekly_report_tuple = WeeklyReport(period=date.today(), summary=weekly_report.summary)
+    db.add(weekly_report_tuple)
+    db.flush()
+    for i in range(len(weekly_analyses)):
+        weekly_analyses[i].weekly_report_id = weekly_report_tuple.id
+
     logging.info(f"주간 분석 cron job 실행 완료: {date.today()}")
     db.commit()
--- app/main.py
+++ app/main.py
@@ -12,11 +12,11 @@ from app.job.job_manager import JobManager
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    job_manager.register_jobs(trade_report_job, CronTrigger(hour=0, minute=0), "trade_report_job")
-    job_manager.register_jobs(weekly_report_job, CronTrigger(day_of_week="sun", hour=0, minute=0), "weekly_report_job")
-    job_manager.register_jobs(monthly_report_job, CronTrigger(day="1", hour=0, minute=0), "monthly_report_job")
-    job_manager.register_jobs(monthly_stock_report_job, CronTrigger(day="1", hour=0, minute=5),
-                              "monthly_stock_report_job")
+    # job_manager.register_jobs(trade_report_job, CronTrigger(hour=0, minute=0), "trade_report_job")
+    # job_manager.register_jobs(weekly_report_job, CronTrigger(day_of_week="sun", hour=0, minute=0), "weekly_report_job")
+    # job_manager.register_jobs(monthly_report_job, CronTrigger(day="1", hour=0, minute=0), "monthly_report_job")
+    # job_manager.register_jobs(monthly_stock_report_job, CronTrigger(day="1", hour=0, minute=5),
+    #                           "monthly_stock_report_job")
     job_manager.start_scheduler()
     yield
     job_manager.shutdown_scheduler()
@@ -43,6 +43,7 @@ job_manager = JobManager(app)
 
 @app.get("/health")
 async def health_check():
+    await weekly_report_job(app)
     return {"code": 200, "status": "healthy"}
 
 
